d702e768f28ecd44e2719c83d0090dd9fbc28bf2
Emmanuel William Morel <manorathan@gmail.com>
2017-11-10 03:10:41 -0600

    Manual edits

--- gsv_object_counter/gsv_object_counter/Results.py
+++ gsv_object_counter/gsv_object_counter/Results.py
@@ -4,10 +4,13 @@ import argparse
 import os
 import sys
 import json
+import multiprocessing as mproc
 
 
 parser = argparse.ArgumentParser(description='Summarise findings')
 DATA_FOLDER = '../data'
+
+OBJECTS = ['car', 'person', 'bicycle', 'bus', 'truck', 'motorbike']
 
 # Required positional argument
 parser.add_argument('input_file', type=str,
@@ -16,139 +19,121 @@ parser.add_argument('input_file', type=str,
 parser.add_argument('-o',
                     help='output file')
 
+def fill_true_values(calc_dict, df_ground_truth, df_pred, thres):
+    dct_val = dict()
+    for idx, row in df_ground_truth.iterrows():
+        for obj in OBJECTS:
+            nobj = row[obj]
+            npred = df_pred.loc[(df_pred.Image == row['id']) & (df_pred.Object == obj) , 'count'].values[0] if (df_pred[(df_pred['Image'] == row['id']) & (df_pred['Object'] == obj) & (df_pred.Threshold == thres)].shape[
+                           0] > 0) else 0
+
+            if obj not in dct_val:
+                dct_val[obj] = dict()            
+            dct_val[obj]['TP'] = dct_val[obj].get('TP', 0) + min(npred, nobj)
+
+            dct_val[obj]['FP'] = dct_val[obj].get('FP', 0) + (npred - min(nobj, npred))
+
+            dct_val[obj]['BTP'] = dct_val[obj].get('BTP', 0) + (1 if npred > 0 and nobj > 0 else 0)
+
+            dct_val[obj]['BFP'] = dct_val[obj].get('BFP', 0) + (1 if npred > 0 and nobj == 0 else 0)
+
+            dct_val[obj]['TN'] = dct_val[obj].get('TN', 0) + (1 if npred == nobj == 0 else 0)
+
+            dct_val[obj]['FN'] = dct_val[obj].get('FN', 0) + (nobj - min(npred, nobj))
+
+            dct_val[obj]['BFN'] = dct_val[obj].get('BFN', 0) + (1 if npred == 0 and nobj > 0 else 0)
+
+    calc_dict[str(thres)] = dct_val
+    print("Finished filling truth rate for threshold " + str(thres))
+    return
+
+
+
 def confusion_matrix(infile, outfile):
 
-    ground_truth = pd.read_csv(infile, header=0)
-    pred = pd.read_csv(outfile)
-    failids = np.genfromtxt(os.path.join(DATA_FOLDER, 'in', 'fail.csv'), dtype=int).tolist()
-    ground_truth = ground_truth[~ground_truth.id.isin(failids)]
-    ground_truth.replace(-1, 0, inplace=True)
+    mgr = mproc.Manager()
+
+    df_ground_truth_source = pd.read_csv(infile, header=0)
+    df_pred = pd.read_csv(outfile)
+    df_pred = df_pred.loc[(df_pred.Object.isin(OBJECTS))]
+
+    # failids = np.genfromtxt(os.path.join(DATA_FOLDER, 'in', 'fail.csv'), dtype=int).tolist()
+    # df_ground_truth_source = df_ground_truth_source[~df_ground_truth_source.id.isin(failids)]
+    df_ground_truth_source.replace(-1, 0, inplace=True)
+
+    df_ground_truth = pd.DataFrame(columns = ['id', 'car', 'person', 'bicycle', 'bus', 'truck', 'motorbike'])
+
+
+    df_ground_truth = df_ground_truth.append([{'id': row['id'],
+                             'car': row['Cars'],
+                             'person': row['Ped'] + row['Cyclists'],
+                             'bicycle': row['Cyclists'] + row['Parked_cycles'],
+                             'bus': row['Buses'],
+                             'truck': row['Vans'],
+                             'motorbike': row['Motorbikes']
+                             } for idx, row in df_ground_truth_source.iterrows()], ignore_index=True)
+
+
 
     result = dict()
 
     result_sample = dict()
 
-    calc_dict = dict()
+    dct_calc = mgr.dict()
 
-    objects = ['car', 'person', 'bicycle', 'bus', 'truck', 'motorbike']
+    dct_neg_pos = dict()
 
-    for idx, row in ground_truth.iterrows():
+    lst_threshold = np.arange(0.0, 1.05, 0.025).tolist()
 
-        for obj in objects:
-            nobj = 0
-            if obj == 'person':
-                nobj = row['Ped'] + row['Cyclists']
-            if obj == 'bicycle':
-                nobj = row['Cyclists'] + row['Parked_cycles']
-            if obj == 'bus':
-                nobj = row['Buses']
-            if obj == 'truck':
-                nobj = row['Vans']
-            if obj == 'motorbike':
-                nobj = row['Motorbikes']
-            if obj == 'car':
-                nobj = row['Cars']
+    lst_proc = []
 
-            npred = pred.loc[(pred.Image == row['id']) & (pred.Object == obj), 'count'].values[0] if (pred[(pred['Image'] == row['id']) & (pred['Object'] == obj)].shape[0] > 0) else 0
+    for thres in lst_threshold:
+        proc = mproc.Process(target=fill_true_values,
+                             args=(dct_calc, 
+                                   df_ground_truth,
+                                   df_pred.loc[df_pred.Threshold == thres],
+                                   thres,
+                                   )
+                             )
+        proc.start()
 
-            if obj not in calc_dict:
-                calc_dict[obj] = dict()
+        lst_proc.append(proc)
 
-            calc_dict[obj]['TP'] = calc_dict[obj].get('TP', 0) + min(npred, nobj)
+    for obj in OBJECTS:
+        dct_neg_pos[obj] = dict()
+        dct_neg_pos[obj]['N'] = df_ground_truth[(df_ground_truth[obj] == 0)].shape[0]
+        dct_neg_pos[obj]['P'] = df_ground_truth[obj].sum()
+        dct_neg_pos[obj]['BP'] = df_ground_truth[(df_ground_truth[obj] > 0)].shape[0]
 
-            calc_dict[obj]['FP'] = calc_dict[obj].get('FP', 0) + (npred - min(nobj, npred))
+    for proc in lst_proc:
+        proc.join()
 
-            calc_dict[obj]['BTP'] = calc_dict[obj].get('BTP', 0) + (1 if npred > 0 and nobj > 0 else 0)
+    df_result_sample = pd.DataFrame(columns=['Class', 'Threshold', 'Specificity', 'Sensitivity', 'Sensitivity_b'])
+    df_result = pd.DataFrame(columns=['Class', 'Threshold', 'Specificity', 'Specificity_b', 'Sensitivity', 'Sensitivity_b'])
 
-            calc_dict[obj]['BFP'] = calc_dict[obj].get('BFP', 0) + (1 if npred > 0 and nobj == 0 else 0)
+    for thres in lst_threshold:
+        df_result_sample = df_result_sample.append([{'Class': obj,
+                                  'Threshold': str(thres),
+                                  'Sensitivity': (dct_calc[str(thres)][obj]['TP']/ dct_neg_pos[obj]['P'] if dct_neg_pos[obj]['P'] > 0 else 0),
+                                  'Sensitivity_b': (dct_calc[str(thres)][obj]['BTP'] / dct_neg_pos[obj]['BP'] if dct_neg_pos[obj]['BP'] > 0 else 0),
+                                 'Specificity': (dct_calc[str(thres)][obj]['TN'] / dct_neg_pos[obj]['N'] if dct_neg_pos[obj]['N'] > 0 else 0)}
+                                 for obj in OBJECTS], ignore_index=True)
 
-            calc_dict[obj]['TN'] = calc_dict[obj].get('TN', 0) + (1 if npred == nobj == 0 else 0)
+        df_result = df_result.append([{'Class': obj,
+                           'Threshold': str(thres),
+                           'Sensitivity': (dct_calc[str(thres)][obj]['TP'] / (dct_calc[str(thres)][obj]['TP'] + dct_calc[str(thres)][obj]['FN']) if (dct_calc[str(thres)][obj]['TP'] + dct_calc[str(thres)][obj]['FN']) > 0 else 0),
+                           'Sensitivity_b': ((dct_calc[str(thres)][obj]['BTP'] / (dct_calc[str(thres)][obj]['BTP'] + dct_calc[str(thres)][obj]['FN'])) if (dct_calc[str(thres)][obj]['BTP'] + dct_calc[str(thres)][obj]['FN']) > 0 else 0),
+                           'Specificity': ((dct_calc[str(thres)][obj]['TN'] / (dct_calc[str(thres)][obj]['TN'] + dct_calc[str(thres)][obj]['FP'])) if (dct_calc[str(thres)][obj]['TN'] + dct_calc[str(thres)][obj]['FP']) > 0 else 0),
+                           'Specificity_b': ((dct_calc[str(thres)][obj]['TN'] / (dct_calc[str(thres)][obj]['TN'] + dct_calc[str(thres)][obj]['BFP'])) if (dct_calc[str(thres)][obj]['TN'] + dct_calc[str(thres)][obj]['FP']) > 0 else 0)}
+                        for obj in OBJECTS], ignore_index=True)
 
-            calc_dict[obj]['FN'] = calc_dict[obj].get('FN', 0) + (nobj - min(npred, nobj))
 
-            calc_dict[obj]['BFN'] = calc_dict[obj].get('BFN', 0) + (1 if npred == 0 and nobj > 0 else 0)
+    # result = pd.DataFrame([(obj, stat, val) for obj, value in result.items() for stat, val in value.items()], columns=['Class', 'Statistic', 'Val'])
+    # result_sample = pd.DataFrame([(obj, stat, val) for obj, value in result_sample.items() for stat, val in value.items()],
+    #                       columns=['Class', 'Statistic', 'Val'])
 
-
-    for obj in calc_dict.keys():
-
-        if obj == 'person':
-            calc_dict[obj]['N'] = ground_truth[(ground_truth['Ped'] == 0) | (ground_truth['Cyclists'] == 0)].shape[0]
-            calc_dict[obj]['P'] = ground_truth['Ped'].sum() + ground_truth['Cyclists'].sum()
-            calc_dict[obj]['PB'] = ground_truth[(ground_truth['Ped'] > 0) | (ground_truth['Cyclists'] > 0)].shape[0]
-        if obj == 'bicycle':
-            calc_dict[obj]['N'] = ground_truth[(ground_truth['Cyclists'] == 0) | (ground_truth['Parked_cycles'] == 0)].shape[0]
-            calc_dict[obj]['P'] = ground_truth['Cyclists'].sum() + ground_truth['Parked_cycles'].sum()
-            calc_dict[obj]['PB'] = ground_truth[(ground_truth['Cyclists'] > 0) | (ground_truth['Parked_cycles'] > 0)].shape[0]
-        if obj == 'bus':
-            calc_dict[obj]['N'] = ground_truth[ground_truth['Buses'] == 0].shape[0]
-            calc_dict[obj]['P'] = ground_truth['Buses'].sum()
-            calc_dict[obj]['PB'] = ground_truth[ground_truth['Buses'] > 0].shape[0]
-        if obj == 'truck':
-            calc_dict[obj]['N'] = ground_truth[ground_truth['Vans'] == 0].shape[0]
-            calc_dict[obj]['P'] = ground_truth['Vans'].sum()
-            calc_dict[obj]['PB'] = ground_truth[ground_truth['Vans'] > 0].shape[0]
-        if obj == 'motorbike':
-            calc_dict[obj]['N'] = ground_truth[ground_truth['Motorbikes'] == 0].shape[0]
-            calc_dict[obj]['P'] = ground_truth['Motorbikes'].sum()
-            calc_dict[obj]['PB'] = ground_truth[ground_truth['Motorbikes'] > 0].shape[0]
-        if obj == 'car':
-            calc_dict[obj]['N'] = ground_truth[ground_truth['Cars'] == 0].shape[0]
-            calc_dict[obj]['P'] = ground_truth['Cars'].sum()
-            calc_dict[obj]['PB'] = ground_truth[ground_truth['Cars'] > 0].shape[0]
-
-        result[obj] = dict()
-
-        result_sample[obj] = dict()
-
-        result_sample[obj]['sensitivity'] = calc_dict[obj]['TP'] / calc_dict[obj]['P'] if (calc_dict[obj]['P'] > 0) else 0
-
-        result_sample[obj]['sensitivity_b'] = calc_dict[obj]['BTP'] / calc_dict[obj]['PB']  if (calc_dict[obj]['PB'] > 0) else 0
-
-        result_sample[obj]['specificity'] = calc_dict[obj]['TN'] / calc_dict[obj]['N'] if (calc_dict[obj]['N'] > 0) else 0
-
-        print(obj + 'TP:' + str(calc_dict[obj]['TP']))
-        print(obj + 'TP + FN:' + str(calc_dict[obj]['BTP'] + calc_dict[obj]['FN']))
-        print(obj + 'P:' + str(calc_dict[obj]['P']) + 'PB' + str(calc_dict[obj]['PB']))
-        print(obj + 'N:' + str(calc_dict[obj]['N']))
-
-        result[obj]['sensitivity'] = (calc_dict[obj]['TP'] / (calc_dict[obj]['TP'] + calc_dict[obj]['FN'])) if (
-                                                                                                               calc_dict[
-                                                                                                                   obj][
-                                                                                                                   'TP'] +
-                                                                                                               calc_dict[
-                                                                                                                   obj][
-                                                                                                                   'FN']) > 0 else 0
-
-        result[obj]['sensitivity_b'] = (calc_dict[obj]['BTP'] / (calc_dict[obj]['BTP'] + calc_dict[obj]['FN'])) if (
-                                                                                                                   calc_dict[
-                                                                                                                       obj][
-                                                                                                                       'BTP'] +
-                                                                                                                   calc_dict[
-                                                                                                                       obj][
-                                                                                                                       'BFN']) > 0 else 0
-
-        result[obj]['specificity'] = (calc_dict[obj]['TN'] / (calc_dict[obj]['TN'] + calc_dict[obj]['FP'])) if (
-                                                                                                               calc_dict[
-                                                                                                                   obj][
-                                                                                                                   'TN'] +
-                                                                                                               calc_dict[
-                                                                                                                   obj][
-                                                                                                                   'FP']) > 0 else 0
-
-        result[obj]['specificity_b'] = (calc_dict[obj]['TN'] / (calc_dict[obj]['TN'] + calc_dict[obj]['BFP'])) if (
-                                                                                                                  calc_dict[
-                                                                                                                      obj][
-                                                                                                                      'TN'] +
-                                                                                                                  calc_dict[
-                                                                                                                      obj][
-                                                                                                                      'BFP']) > 0 else 0
-
-    result = pd.DataFrame([(obj, stat, val) for obj, value in result.items() for stat, val in value.items()], columns=['Class', 'Statistic', 'Val'])
-    result_sample = pd.DataFrame([(obj, stat, val) for obj, value in result_sample.items() for stat, val in value.items()],
-                          columns=['Class', 'Statistic', 'Val'])
-
-    result.to_csv(os.path.join(DATA_FOLDER, 'out', 'summary.csv'), index=False, header=True)
-    result_sample.to_csv(os.path.join(DATA_FOLDER, 'out', 'summary_sample.csv'), index=False, header=True)
+    df_result_sample.to_csv(os.path.join(DATA_FOLDER, 'out', 'summary_sample.csv'), index=False, header=True)
+    df_result.to_csv(os.path.join(DATA_FOLDER, 'out', 'summary.csv'), index=False, header=True)
 
     return
 
